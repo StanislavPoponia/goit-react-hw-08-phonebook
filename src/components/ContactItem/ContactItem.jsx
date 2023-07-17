c25a059fa404f31daee8477ca60fb1452e68314b
StanislavPoponia <127778439+StanislavPoponia@users.noreply.github.com>
2023-07-17 09:50:09 +0300

    new

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,94 +1,37 @@
 import { PropTypes } from 'prop-types';
 import 'react-toastify/dist/ReactToastify.css';
-import { RotatingLines } from 'react-loader-spinner';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { toast } from 'react-toastify';
 
 import ButtonStyled from '../Button/ButtonStyled';
 import {
   useDeleteContactMutation,
-  useUpdateContactMutation,
+  
 } from 'redux/contactsSlice';
 import css from './ContactItem.module.css';
 
-const ContactItem = ({ id, name, number }) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedName, setEditedName] = useState(name);
-  const [editedNumber, setEditedNumber] = useState(number);
-
+  const ContactItem = ({ id, name, number }) => {
   const [deleteContact, { isLoading: isDeleting, isSuccess: isDeleted }] =
     useDeleteContactMutation();
-  const [updateContact, { isLoading: isUpdating, isSuccess: isUpdated }] =
-    useUpdateContactMutation();
-
+ 
   useEffect(() => {
     if (isDeleted) {
       toast.success('Contact has been deleted!');
     }
-    if (isUpdated) {
-      setIsEditing(false);
-      toast.success('Contact has been updated!');
-    }
-  }, [isDeleted, isUpdated]);
-
-  const handleEditClick = () => {
-    setIsEditing(true);
-  };
-
-  const handleCancelClick = () => {
-    setIsEditing(false);
-    setEditedName(name);
-    setEditedNumber(number);
-  };
-
-  const handleSaveClick = () => {
-    updateContact({ id, name: editedName, number: editedNumber });
-  };
+   
+  }, [isDeleted]);
 
   const handleDeleteClick = () => { 
-      deleteContact(id);
-  };
-
-  const handleChange = e => {
-    if (e.target.name === 'name') {
-      setEditedName(e.target.value);
-    } else if (e.target.name === 'number') {
-      setEditedNumber(e.target.value);
-    }
-  };
-
+      deleteContact(id); };
   return (
     <li className={css.contactItem}>
-      {isEditing ? (
-        <>
-          <input
-            type="text"
-            name="name"
-            value={editedName}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="number"
-            value={editedNumber}
-            onChange={handleChange}
-          />
-          <div>
-            <ButtonStyled onClick={handleSaveClick} disabled={isUpdating}>
-              {isUpdating && <RotatingLines height="18" width="18" />}
-              Save
-            </ButtonStyled>
-            <ButtonStyled onClick={handleCancelClick}>Cancel</ButtonStyled>
-          </div>
-        </>
-      ) : (
+   {(
         <>
           <span>{name}:</span> 
           <span>{number}</span>
           <div>
-            <ButtonStyled onClick={handleEditClick}>Edit</ButtonStyled>
             <ButtonStyled onClick={handleDeleteClick} disabled={isDeleting}>
-              {isDeleting && <RotatingLines height="18" width="18" />}
+             
               Delete
             </ButtonStyled>
           </div>
